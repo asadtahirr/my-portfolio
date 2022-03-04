59021c612a398a869e04b926eeb4eb09c8b028d8
Mianasad3 <asadtahir9999@gmail.com>
2022-03-04 10:11:18 +0530

    add navbar

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("working");
+const hamburger = document.querySelector(".navbar-hamburger");
+const nav = document.querySelector(".navbar-nav");
+
+function toggleMenu(e) {
+  nav.classList.toggle("active");
+}
+
+hamburger.addEventListener("click", toggleMenu);
